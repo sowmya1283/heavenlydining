ff6ba5ac7a04690bc0a1e6624ce8b051228a7823
sowmya1283 <75764956+sowmya1283@users.noreply.github.com>
2025-06-22 23:58:39 +0100

    created required folders and added static import to urls.py

--- uniquedining/urls.py
+++ uniquedining/urls.py
@@ -18,8 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from booktable.views import booking
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('booking/', booking, name='booking'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
