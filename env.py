0d6c0810080150c7cf20c842aff8d33490125312
sowmya1283 <75764956+sowmya1283@users.noreply.github.com>
2025-06-25 14:06:42 +0100

    Added secretkeyin env.py and settings.py file

--- env.py
+++ env.py
@@ -2,4 +2,4 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_NbyBRzpQ5S8O@ep-lively-salad-a26auvb0.eu-central-1.aws.neon.tech/drift_ouch_rigid_43882")
-
+os.environ.setdefault("SECRET_KEY", "django-insecure-i+m#ctpv!*6b_yau0=#xw^8=4o2^g%26ms%q@jobsi1c!(wq0m")
