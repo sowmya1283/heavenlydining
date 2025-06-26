977eeb484c939dccfdef0256ddff7de0379c7593
sowmya1283 <75764956+sowmya1283@users.noreply.github.com>
2025-06-26 14:45:31 +0100

    Added booking table to the databse

--- booktable/models.py
+++ booktable/models.py
@@ -9,3 +9,17 @@ class UserProfile(models.Model):
     displayname = models.CharField(max_length=150)
     phone_number = models.CharField(max_length=20, blank=True, null=True)
     address = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.displayname
+
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_bookings")
+    table_size = models.PositiveIntegerField()
+    booking_date = models.DateField()
+    booking_time = models.TimeField()
+    llergies = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"Booking by {self.user.usename} on {self.booking.date} at {self.booking_time} for {self.table_size} people" 
