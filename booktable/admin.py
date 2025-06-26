39a21b5bd93a8a3ed97cde8c448f06b0d948efdc
sowmya1283 <75764956+sowmya1283@users.noreply.github.com>
2025-06-26 15:06:12 +0100

    Added booking table to admin site

--- booktable/admin.py
+++ booktable/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import UserProfile
+from .models import Booking
 
 
 # Register your models here.
 admin.site.register(UserProfile)
+admin.site.register(Booking)
