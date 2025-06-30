27cda044b5293ca086c49cab1f17f314b42843de
sowmya1283 <75764956+sowmya1283@users.noreply.github.com>
2025-06-30 07:35:34 +0100

    Added account registration code, modified login and logout

--- uniquedining/urls.py
+++ uniquedining/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/", include("allauth.urls")),
     path('summernote/', include('django_summernote.urls')),
     path("", include("booktable.urls"), name="booktable-urls"), 
 
